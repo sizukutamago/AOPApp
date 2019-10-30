40729574807e87c2bd98366958e7618b9e6f7378
sizukutamago <tanaka@tam-bourine.co.jp>
2019-10-31 01:45:32 +0900

    aspectj-autoproxy

--- src/main/java/com/aop/MyBeanAspect.java
+++ src/main/java/com/aop/MyBeanAspect.java
@@ -1,11 +1,15 @@
 package com.aop;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 
+@Aspect
 public class MyBeanAspect {
 
+    @Before("execution(* com.aop.IMyBean.addData(..))")
     public void addDataBefore(JoinPoint joinPoint) {
-        System.out.println("*addData before...");
+        System.out.println("*addData before...*");
 
         String args = "args: \"";
         for (Object ob : joinPoint.getArgs()) {
